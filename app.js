8e96c98d6b3a9b8597e89e79519bd87d37de1ed6
sohamdawn777 <sohamdawn12345@gmail.com>
2025-08-31 12:03:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,8 @@ window.onload = () => {
     <p>This is sample text.</p>
   `, { maxWidth: 200, minWidth: 50, autoPan: true, closeButton: true, keepInView: true });
 
-  marker.on("click", function() { this.openPopup(); });
+  marker.on("click", function() { this.openPopup();
+document.getElementById("ar-btn").addEventListener("click", viewAR) });
 
   L.circle([22.526911,88.377648], { radius: 15, color: "blue", fillColor: "blue", fillOpacity: 0.2 }).addTo(map);
 
