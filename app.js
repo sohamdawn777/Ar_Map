b5a033796f4e177cf689c844267ab9af6c609a15
sohamdawn777 <sohamdawn12345@gmail.com>
2025-08-31 20:45:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,8 +18,23 @@ function log(msg) {
   el.innerHTML += msg + "<br>";
 }
 
+window.addEventListener("DOMContentLoaded", () => {
+  const map = L.map("map", { center:[22.526911,88.377648], zoom: 19, maxZoom: 19, minZoom: 1 });
 
-async function viewAR (event) {
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", { maxZoom:19, minZoom:1, tms: false }).addTo(map);
+
+  const marker = L.marker([22.526911,88.377648], { 
+    icon: L.icon({ iconUrl: "graffitiIco.png", iconSize: [32,32], iconAnchor: [16,32], popupAnchor: [0,-32] }),
+    title: "Graffiti Spot", draggable: false, riseOnHover: true, riseOffset: 250
+  }).addTo(map);
+
+  marker.bindPopup(`
+    <p>This is sample text.</p><button type="button" id="ar-btn">View in AR</button>`, { maxWidth: 200, minWidth: 50, autoPan: true, closeButton: true, keepInView: true });
+
+  marker.on("popupopen", function(e) {
+  const btn=e.popup._contentNode.querySelector("button");
+if (btn && !btn.listenerAdded) {
+btn.addEventListener("click",() => {
 log("viewAR called.");
 
 const existing=document.querySelector("a-scene");
@@ -49,25 +64,7 @@ aScene.appendChild(aCamera);
 document.body.appendChild(aScene);
 
 aScene.scrollIntoView({behavior:"smooth"});
-}
-
-window.addEventListener("DOMContentLoaded", () => {
-  const map = L.map("map", { center:[22.526911,88.377648], zoom: 19, maxZoom: 19, minZoom: 1 });
-
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", { maxZoom:19, minZoom:1, tms: false }).addTo(map);
-
-  const marker = L.marker([22.526911,88.377648], { 
-    icon: L.icon({ iconUrl: "graffitiIco.png", iconSize: [32,32], iconAnchor: [16,32], popupAnchor: [0,-32] }),
-    title: "Graffiti Spot", draggable: false, riseOnHover: true, riseOffset: 250
-  }).addTo(map);
-
-  marker.bindPopup(`
-    <p>This is sample text.</p><button type="button" id="ar-btn">View in AR</button>`, { maxWidth: 200, minWidth: 50, autoPan: true, closeButton: true, keepInView: true });
-
-  marker.on("popupopen", function(e) {
-  const btn=e.popup._contentNode.querySelector("button");
-if (btn && !btn.listenerAdded) {
-btn.addEventListener("click",() => viewAR());
+});
 btn.listenerAdded= true;
 }
 });
